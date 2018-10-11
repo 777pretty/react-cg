4406028bb5aa47f53037cdc56ea284caf95032a6
777z <7b.onus583@gmail.com>
2018-10-11 22:01:38 +0200

    refApiMod2

--- src/hoc/Zklasou.js
+++ src/hoc/Zklasou.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React, { Component } from 'react'
 
 const zklasou = (ObalenyKomponent, nazovKlasy) => {
-    return (props) => (
-    <div className={nazovKlasy}>
-        <ObalenyKomponent ref={props.posunutaRef} {...props} />
-    </div>
+    const ZKlasou = class extends Component {
+    render(){
+        return(
+         <div className={nazovKlasy}>
+               <ObalenyKomponent ref={this.props.posunutaRef} {...this.props} />
+         </div>
     )
+    }
+}
+    return React.posunutaRef((props, ref) => {
+        return <ZKlasou {...props} posunutaRef={ref} />
+    })
 }
 
 export default zklasou
